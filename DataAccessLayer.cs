e1e2ad348542b3c6006e88e2bb703746e4d7000e
Aastha Goyal <aastha2150@gmail.com>
2019-03-01 10:27:28 +1300

    First commit

---
+++ DataAccessLayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace TopStarWebApplication01
+{
+    public class DataAccessLayer
+    {
+        public SqlConnection connection = null;
+        String connectionString = null;
+       
+
+        public void AddPanelIems(string logo, string companyName, string img1, string img2, string img3, int navid, string address, string phoneNo)
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["dbtopstarConnectionString"].ConnectionString;
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            String getNavid = "select n.navId from tbnavigation n , tbPanel p where n.panelid = p.panelId ";
+            SqlCommand cmd = new SqlCommand(getNavid, connection);
+            int navigationId = Convert.ToInt32(cmd.ExecuteScalar());
+
+
+            String query = "insert into tbPanel values ('" + img1 + "','" + img2 + "','" + img3 + "','" + logo + "','" + navigationId + "','" + companyName + "','" + address+ "')";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.ExecuteNonQuery();
+
+
+
+        }
+    }
+}
---
+++ DemoImage.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DemoImage.aspx.cs" Inherits="TopStarWebApplication01.DemoImage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <asp:FileUpload ID="fpLogoImage" runat="server" Height="25px" Width="269px" />
+&nbsp;<asp:button id="btnUpload" type="submit" text="Upload" runat="server" Height="31px" Width="119px" OnClick="btnUpload_Click"></asp:button>
+            <asp:Panel ID="frmConfirmation" Visible="False" Runat="server">
+<asp:Label id="txtResult" Runat="server"/>
+</asp:Panel>
+        </div>
+    </form>
+</body>
+</html>
